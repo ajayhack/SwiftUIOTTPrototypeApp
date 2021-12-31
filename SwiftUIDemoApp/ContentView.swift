1ff143f45f6a0776c99026ca2b9f5049c11d2220
Ajay Thakur <ajay49811@gmail.com>
2021-12-31 19:19:33 +0530

    Initial Commit

---
+++ SwiftUIDemoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemoApp
+//
+//  Created by Ajay Thakur on 31/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIDemoApp/SwiftUIDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoAppApp.swift
+//  SwiftUIDemoApp
+//
+//  Created by Ajay Thakur on 31/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
